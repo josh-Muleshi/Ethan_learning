d9111f0721f08c54456c743a3e4c39a4524cb203
josh muleshi <jmuleshi2@gmail.com>
2025-01-24 09:22:44 +0000

    all doctrine test done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
