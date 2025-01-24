6877befbdf5c9b8ac400b297498c2051423c4cad
josh muleshi <jmuleshi2@gmail.com>
2025-01-24 12:41:57 +0000

    last commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,14 +20,7 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', ['users' => $users]);
     }
 
-    #[Route('/user/{id}', name:'user.show')]
-    public function show(int $id, UserRepository $userRepository): Response
-    {
-        $user = $userRepository->find($id);
-        return $this->render('user/show.html.twig', ['user' => $user]);
-    }
-
-    #[Route('/user/{id}/edit', name:'user.edit')]
+    #[Route('/user/{id}/edit', name:'user.edit', methods: ['GET', 'POST'])]
     public function edit(User $user, Request $request, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -41,5 +34,38 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/user/create', name:'user.create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success','L\'utilisateur a bien été créé');
+            return $this->redirectToRoute('user.index');
+        }
+        return $this->render('user/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/user/{id}', name:'user.show')]
+    public function show(int $id, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        return $this->render('user/show.html.twig', ['user' => $user]);
+    }
+
+    #[Route('/user/{id}/edit', name:'user.delete', methods: ['DELETE'])]
+    public function delete(User $user, EntityManagerInterface $em): Response
+    {
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success','L\'utilisateur a bien été supprimé');
+        return $this->redirectToRoute('user.index');
     }
 }
