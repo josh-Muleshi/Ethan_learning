e60ebb99ce537105ec460aa0767c0021dcd14303
josh muleshi <jmuleshi2@gmail.com>
2025-01-24 11:52:54 +0000

    all mod

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\Entity\User;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,5 +25,21 @@ class UserController extends AbstractController
     {
         $user = $userRepository->find($id);
         return $this->render('user/show.html.twig', ['user' => $user]);
+    }
+
+    #[Route('/user/{id}/edit', name:'user.edit')]
+    public function edit(User $user, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success','L\'utilisateur a bien été modifié');
+            return $this->redirectToRoute('user.index');
+        }
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('user_name', null, ['label' => 'Nom d\'utilisateur'])
+            ->add('password', null, ['label' => 'Mot de passe'])
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+                'label'=> 'Date de création',
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
